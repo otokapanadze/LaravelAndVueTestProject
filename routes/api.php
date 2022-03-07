a80500887b7d728c054aa7f529b8325cfc10801f
Oto Kapanadze <otokapanadze@icloud.com>
2022-03-07 19:00:24 +0400

    Moved product and authors api routes to sanctum middleware

--- routes/api.php
+++ routes/api.php
@@ -14,13 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-//Route::post('login', [\App\Http\Controllers\UserController::class ,'login']);
-//Route::post('logout', [\App\Http\Controllers\UserController::class ,'logout']);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+    Route::resource('products', \App\Http\Controllers\ProductController::class);
+    Route::resource('authors', \App\Http\Controllers\AuthorController::class);
 });
-
-
-Route::resource('products', \App\Http\Controllers\ProductController::class);
-Route::resource('authors', \App\Http\Controllers\AuthorController::class);
