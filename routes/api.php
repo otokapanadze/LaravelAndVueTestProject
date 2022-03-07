bda70415a865fa56f9d5fe831e00b3df67dfb1eb
Oto Kapanadze <otokapanadze@icloud.com>
2022-03-07 18:26:26 +0400

    Added some sanctum configurations

--- routes/api.php
+++ routes/api.php
@@ -14,9 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//    return $request->user();
-//});
+//Route::post('login', [\App\Http\Controllers\UserController::class ,'login']);
+//Route::post('logout', [\App\Http\Controllers\UserController::class ,'logout']);
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
 
 
 Route::resource('products', \App\Http\Controllers\ProductController::class);
